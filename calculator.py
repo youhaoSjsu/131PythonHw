59d7e27e198179a3426657e668927a90e11d8a30
youhaoSjsu <89713281+youhaoSjsu@users.noreply.github.com>
2021-10-13 19:59:38 -0700

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -1,38 +1,4 @@
 import math
-
-
-def parse_input():
-    """
-        get the text of user input and parse the text.
-
-        This function simply split user's input into 3 parts: number1, number2, and operator.
-        pass these variables to next function.
-        Parameters
-        ----------
-        Returns
-        -------
-        false if number1 or number2 or not numbers
-
-        Examples
-        --------
-        >> 10 + 11
-        number1: 10 ,number2: 11, operator: +
-        """
-    number1 = 0.0
-    number2 = 0.0
-    print("Enter equation: ")
-    text = input()
-    # split text into three variables by " ", So that we can get the data
-    strList = text.split(" ")
-    # prevent system get error when user input wrong text
-    try:
-        number1 = float(strList[0])
-        number2 = float(strList[2])
-    except ValueError:
-        return False
-    calculator(number1, number2, strList[1])
-
-
 def calculator(number1, number2, operator):
     """
             make calculation
@@ -72,6 +38,40 @@ def calculator(number1, number2, operator):
     else:
         return False
     print(result)
+
+def parse_input():
+    """
+        get the text of user input and parse the text.
+
+        This function simply split user's input into 3 parts: number1, number2, and operator.
+        pass these variables to next function.
+        Parameters
+        ----------
+        Returns
+        -------
+        false if number1 or number2 or not numbers
+
+        Examples
+        --------
+        >> 10 + 11
+        number1: 10 ,number2: 11, operator: +
+        """
+    number1 = 0.0
+    number2 = 0.0
+    print("Enter equation: ")
+    text = input()
+    # split text into three variables by " ", So that we can get the data
+    strList = text.split(" ")
+    # prevent system get error when user input wrong text
+    try:
+        number1 = float(strList[0])
+        number2 = float(strList[2])
+    except ValueError:
+        return False
+    calculator(number1, number2, strList[1])
+
+
+
 
 
 parse_input()
